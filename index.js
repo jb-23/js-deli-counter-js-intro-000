5284e151b4024480457a5a472993249ed8a21990
Jason Bamford <scarfandgoggles@yahoo.co.uk>
2019-02-23 11:56:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function takeANumber(que, name) {
   n = que.length;
   return `Welcome, ${name}. You are number ${n} in line.`;
 }
+
+function nowServing(que) {
+  if (que.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  var s = que[0];
+  que.shift();
+  return s;
+}
